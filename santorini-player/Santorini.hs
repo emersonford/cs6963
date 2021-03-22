5de0893beaf74fa96d0c58ed2e0e10b1ba3fc491
Emerson Ford <emersontford@gmail.com>
2021-03-22 13:57:39 -0600

    Add first round of optimizations

--- santorini-player/Santorini.hs
+++ santorini-player/Santorini.hs
@@ -136,6 +136,15 @@ mValidGameState gs = if validateGameState gs then Just gs else Nothing
 genCoords :: (Coord -> Bool) -> [Coord]
 genCoords filterF = filter filterF [(x, y) | x <- [1 .. 5], y <- [1 .. 5]]
 
+genCoordsAround :: Coord -> (Coord -> Bool) -> [Coord]
+genCoordsAround (cx, cy) filterF =
+  [ (x, y)
+    | x <- [(max 1 (cx - 1)) .. (min 5 (cx + 1))],
+      y <- [(max 1 (cy - 1)) .. (min 5 (cy + 1))],
+      (x, y) /= (cx, cy),
+      filterF (x, y)
+  ]
+
 genTokens :: (Tokens -> Bool) -> [Tokens]
 genTokens filterF =
   nubBy
@@ -148,12 +157,6 @@ genTokens filterF =
         filterF ((x, y), (x', y'))
     ]
 
-inSurrounding :: Coord -> Coord -> Bool
-inSurrounding (fromX, fromY) (toX, toY) =
-  let xDiff = abs (fromX - toX)
-      yDiff = abs (fromY - toY)
-   in ((xDiff == 1) || (yDiff == 1)) && (xDiff <= 1) && (yDiff <= 1)
-
 canBuild' :: Coord -> Coord -> GameState -> Bool -> Bool
 canBuild' (fromX, fromY) (toX, toY) gs checkOccupied =
   let (GameState _ sp pl) = gs
@@ -161,7 +164,6 @@ canBuild' (fromX, fromY) (toX, toY) gs checkOccupied =
         && ( not checkOccupied
                || not (occupied (toX, toY) (getTokenPair gs))
            )
-        && inSurrounding (fromX, fromY) (toX, toY)
 
 canBuild :: Coord -> Coord -> GameState -> Bool
 canBuild fromC toC gs = canBuild' fromC toC gs True
@@ -184,7 +186,7 @@ wonOrElse gs els =
    in if spaceIdx sp fstT == 3 then [(gs, True)] else els
 
 getBuildMoves' :: Coord -> GameState -> (Int8 -> Int8) -> [(GameState, Bool)]
-getBuildMoves' fromC gs buildF = genCoords (\x -> canBuild fromC x gs) >>= (\x -> [(gs {spaces = mapToSpace buildF (spaces gs) x}, False)])
+getBuildMoves' fromC gs buildF = genCoordsAround fromC (\x -> canBuild fromC x gs) >>= (\x -> [(gs {spaces = mapToSpace buildF (spaces gs) x}, False)])
 
 getBuildMoves :: Coord -> GameState -> [(GameState, Bool)]
 getBuildMoves fromC gs = getBuildMoves' fromC gs (+ 1)
@@ -193,7 +195,7 @@ getMoves :: Coord -> Coord -> GameState -> [(Coord, GameState)]
 getMoves fromC otherC gs =
   let (GameState _ _ (p1, p2)) = gs
    in [ (toC, gsMove)
-        | toC <- genCoords (\x -> canMove fromC x gs),
+        | toC <- genCoordsAround fromC (\x -> canMove fromC x gs),
           let gsMove = gs {players = (p1 {tokens = (toC, otherC)}, p2)}
       ]
 
@@ -205,7 +207,7 @@ getPossibleGameStates' Apollo gs fromC otherC =
       (p2t1, p2t2) = tokens p2
    in do
         -- Generate a list of first moves, including those that swap with an _opponent_ piece.
-        toC <- genCoords (\x -> canMove' fromC x gs False && x /= otherC)
+        toC <- genCoordsAround fromC (\x -> canMove' fromC x gs False && x /= otherC)
         let p1Player = p1 {tokens = (toC, otherC)}
         let gsMove
               | toC == p2t1 = gs {players = (p1Player, p2 {tokens = (fromC, p2t2)})}
@@ -228,7 +230,7 @@ getPossibleGameStates' Artemis gs fromC otherC =
           ( getBuildMoves toC gsMove1
               -- Generate a list of optional second moves.
               ++ ( do
-                     toC2 <- genCoords (\x -> canMove toC x gsMove1 && (x /= fromC))
+                     toC2 <- genCoordsAround fromC (\x -> canMove toC x gsMove1 && (x /= fromC))
                      let gsMove2 = gsMove1 {players = (p1 {tokens = (toC2, otherC)}, p2)}
 
                      wonOrElse
@@ -258,13 +260,13 @@ getPossibleGameStates' Demeter gs fromC otherC =
       gsMove
       -- Generate a list of builds after the first move.
       ( do
-          firstBuildC <- genCoords (\x -> canBuild toC x gsMove)
+          firstBuildC <- genCoordsAround fromC (\x -> canBuild toC x gsMove)
           let gsBuild1 = gsMove {spaces = mapToSpace (+ 1) (spaces gsMove) firstBuildC}
 
           -- Generate a list of optional second builds after the first build.
           (gsBuild1, False) :
             [ (gsBuild1 {spaces = mapToSpace (+ 1) (spaces gsBuild1) secondBuildC}, False)
-              | secondBuildC <- genCoords (\x -> canBuild toC x gsBuild1 && x /= firstBuildC)
+              | secondBuildC <- genCoordsAround fromC (\x -> canBuild toC x gsBuild1 && x /= firstBuildC)
             ]
       )
 getPossibleGameStates' Hephastus gs fromC otherC =
@@ -276,7 +278,7 @@ getPossibleGameStates' Hephastus gs fromC otherC =
       gsMove
       -- Generate a list of builds after the first move.
       ( do
-          firstBuildC <- genCoords (\x -> canBuild toC x gsMove)
+          firstBuildC <- genCoordsAround fromC (\x -> canBuild toC x gsMove)
           let gsBuild1 = gsMove {spaces = mapToSpace (+ 1) (spaces gsMove) firstBuildC}
 
           -- If the space we just built on is less than 3, add an additonal
@@ -290,7 +292,7 @@ getPossibleGameStates' Minotaur gs fromC otherC =
       (p2t1, p2t2) = tokens p2
    in do
         -- Generate a list of first moves, including those that swap with an _opponent_ piece.
-        toC <- genCoords (\x -> canMove' fromC x gs False && x /= otherC)
+        toC <- genCoordsAround fromC (\x -> canMove' fromC x gs False && x /= otherC)
         let p1Player = p1 {tokens = (toC, otherC)}
 
         let pushback = pushBackToken fromC toC
@@ -329,7 +331,7 @@ getPossibleGameStates' Prometheus gs fromC otherC =
 
           -- Generate a list of moves restricted to the level we're currently at.
           let level = spaceIdx sp fromC
-          toC <- genCoords (\x -> canMove fromC x gs && spaceIdx (spaces gsBuild1) x <= level)
+          toC <- genCoordsAround fromC (\x -> canMove fromC x gs && spaceIdx (spaces gsBuild1) x <= level)
           let gsMove = gsBuild1 {players = (p1 {tokens = (toC, otherC)}, p2)}
 
           -- Generate a list of second builds if we didn't win on our move.
